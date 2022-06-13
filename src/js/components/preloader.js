9997f00b2e7f779c9540380fb853bb85131cb461
Maksym <maksymRozhko7@gmail.com>
2022-06-13 14:49:01 +0300

    feat: main screen, adaptive, animation

--- src/js/components/preloader.js
+++ src/js/components/preloader.js
@@ -1,5 +1,6 @@
 function preloader(preloader) {
     const loader = document.querySelector(preloader);
+    const video = document.querySelector('.main-screen__video');
 
     if (loader) {
         setTimeout(() => {
@@ -10,6 +11,8 @@ function preloader(preloader) {
         }, 3500);
         setTimeout(() => {
             loader.remove();
+            video.setAttribute('autoplay', 'autoplay');
+            video.play();
         }, 5500);
     }
 };
