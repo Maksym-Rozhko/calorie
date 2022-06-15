91b4a36444070e6d0975d7dcf957b0f1b7919a52
Maksym <maksymRozhko7@gmail.com>
2022-06-15 21:24:10 +0300

    feat: new section steps, adaptive, init steps slider

--- src/js/components/initSliders.js
+++ src/js/components/initSliders.js
@@ -1,35 +1,29 @@
 import Swiper, { Navigation, Pagination } from 'swiper';
 Swiper.use([Navigation, Pagination]);
 
-new Swiper('.swiper.rating__slider', {
-    slidesPerView: 4.5,
+new Swiper('.swiper.steps-slider', {
+    slidesPerView: 4,
     spaceBetween: 20,
-    loop: true,
 
     navigation: {
-        nextEl: '.slider__controls .rating__btns.swiper-button-next',
-        prevEl: '.slider__controls .rating__btns.swiper-button-prev',
+        nextEl: '.steps-slider__controls .steps-slider__btns.swiper-button-next',
+        prevEl: '.steps-slider__controls .steps-slider__btns.swiper-button-prev',
+    },
+
+    pagination: {
+      el: '.steps-slider__controls .swiper-pagination',
+      clickable: true,
+        renderBullet: function (index, className) {
+        return '<span class="' + className + '">' + (index + 1) + "</span>";
+      },
     },
 
     breakpoints: {
       320: {
         slidesPerView: 1,
       },
-      375: {
-        slidesPerView: 1.5,
-        spaceBetween: 10,
-      },
-      575: {
-        slidesPerView: 2.5,
-        spaceBetween: 10,
-      },
-      1024: {
-        slidesPerView: 3.5,
-        spaceBetween: 10,
-      },
-      1440: {
-        slidesPerView: 4.5,
-        spaceBetween: 20,
+      1280: {
+        slidesPerView: 4,
       }
   },
 });
